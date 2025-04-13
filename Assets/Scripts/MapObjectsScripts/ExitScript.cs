2569a9b680f1fe5c7cdfdbb40c5c3b490c8b8813
DmitrijNazarov <nazarovdmitrij126@gmail.com>
2025-04-13 16:01:30 +0300

    пустыесцены

--- Assets/Scripts/MapObjectsScripts/ExitScript.cs
+++ Assets/Scripts/MapObjectsScripts/ExitScript.cs
@@ -9,9 +9,21 @@ public class ExitScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
             if (this.transform.position.x - GameObject.FindGameObjectWithTag("Player").transform.position.x < radius && this.transform.position.x - GameObject.FindGameObjectWithTag("Player").transform.position.x > -radius && this.transform.position.y - GameObject.FindGameObjectWithTag("Player").transform.position.y < radius && this.transform.position.y - GameObject.FindGameObjectWithTag("Player").transform.position.y > -radius)
             {
-                SceneManager.LoadScene("Betweenlevels");
-                PlayerPrefs.SetInt("floorcounter", PlayerPrefs.GetInt("floorcounter")+1);
-                PlayerPrefs.Save();
+                if (SceneManager.GetActiveScene().name == "LevelScene")
+                {
+                    SceneManager.LoadScene("Betweenlevels");
+                    PlayerPrefs.SetInt("floorcounter", PlayerPrefs.GetInt("floorcounter") + 1);
+                    PlayerPrefs.Save();
+                }
+                else
+                {
+                    if (PlayerPrefs.GetInt("floorcounter")<6)
+
+                    SceneManager.LoadScene("LevelScene");
+
+                    else
+                        SceneManager.LoadScene("LevelScene");
+                }
             }
 }
 }
